ae75aad09f31fa3227957f81588a21b28c3ed57c
Xenócrates Mello <61846361+xenocratesmello@users.noreply.github.com>
2026-04-23 23:45:38 -0300

    Creating the settings.py and constants.py Creating game.py and main.py

---
+++ code/settings.py
@@ -0,0 +1,22 @@
+# settings.py
+
+# D
+DB_NAME = 'db_aliens_attack'
+
+# I
+INITIAL_SCORE = {
+    'Player': 0,
+    'Enemy': 0}
+
+# M
+MENU_OPTION = {
+    'NEW GAME',
+    'SCORE',
+    'INSTRUCTIONS',
+    'SETTINGS',
+    'EXIT'}
+
+# R
+RELATIVE_PATH = {
+    'Level1BG': 'asset/craftpix/background/craftpix-net-724983-ocean-and-clouds-free-pixel-art-backgrounds/Ocean_1',
+    'Level2BG': 'asset/craftpix/background/craftpix-net-724983-ocean-and-clouds-free-pixel-art-backgrounds/Ocean_2'}
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from code.Game import Game
+from code.game import Game
 
 game = Game()
 game.run()
